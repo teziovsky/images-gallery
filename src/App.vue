0dffa0e9e1d717fd5fe01a372b113f747bba4c85
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-02 02:01:24 +0200

    Added Recursive font family

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Recursive:wght@300;400&display=swap");
+
 *,
 *::before,
 *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+}
+
+body {
+    font-family: "Recursive";
 }
 </style>
