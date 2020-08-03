9f1c6f7986aea383bc7a9d844fae65202f934267
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-03 18:16:15 +0200

    Added <a> tag to imageCard to see full resolution image. Added animations on hover.

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,6 +1,14 @@
 <template>
     <li class="imageCard">
         <img class="imageCard__img" :src="image.url_n" :alt="image.title" />
+        <div class="overlay">
+            <a
+                class="imageCard__external"
+                :href="image.url_o"
+                target="_blank"
+                rel="noopener noreferrer"
+            >FULL SIZE IMAGE</a>
+        </div>
     </li>
 </template>
 
@@ -12,26 +20,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/variables.scss";
+
 .imageCard {
+    width: 200px;
+    height: 200px;
+    border-radius: 2px;
     list-style: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 3px;
     position: relative;
+    overflow: hidden;
 
     &__img {
-        height: 200px;
-        width: 200px;
+        height: 100%;
+        width: 100%;
         object-fit: cover;
-        margin: 5px;
-        border-radius: 15px;
+        border-radius: 4px;
         transition: transform 0.3s ease;
+    }
+
+    &__external {
+        position: absolute;
+        border: 2px solid $primary-color;
+        padding: 5px;
+        text-decoration: none;
+        color: $primary-color;
+        font-weight: 500;
+        opacity: 0;
+        transition: opacity 0.5s ease-in;
 
         &:hover {
-            position: relative;
-            border: 1px solid #000;
-            cursor: pointer;
-            transform: scale(1.2);
-            z-index: 2;
+            box-shadow: 0px 0px 3px 0px $primary-color;
         }
     }
+
+    &:hover &__external {
+        opacity: 1;
+    }
+
+    &:hover &__img {
+        filter: blur(5px);
+    }
+}
+
+.overlay {
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: $secondary-color, $alpha: 0.9);
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    transition: opacity 0.3s ease-in;
+    opacity: 0;
+}
+
+.imageCard:hover > .overlay {
+    opacity: 1;
 }
 
 @media (max-width: 576px) {
