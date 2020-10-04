e0cdbdf8806a2d787e593e0a4fd16a5ff520388b
Jakub Soboczyński <teziovsky@gmail.com>
2020-10-04 15:37:59 +0200

    updated config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Images-gallery/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/images-gallery/" : "/",
 };
