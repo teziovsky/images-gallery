302c4ff81bf94bf8497436abeeb0464310df9d0d
Jakub Soboczyński <teziovsky@gmail.com>
2021-08-10 21:55:07 +0200

    Removed unused Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    <router-view />
+    <Home></Home>
   </div>
 </template>
+
+<script>
+import Home from '@/views/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+};
+</script>
 
 <style lang="scss">
 *,
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 import VueImg from 'v-img';
 
 Vue.use(VueImg, {
@@ -9,6 +8,5 @@ Vue.use(VueImg, {
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount('#app');
